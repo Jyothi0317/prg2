83e40c61164ae0b6c7241843da5d04f7c9f5e4da
Jyothi0317 <you@example.com>
2020-07-18 00:53:25 -0700

    templates added

--- prg2/views.py
+++ prg2/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("hello this is project2")
 
 def home(request):
     return HttpResponse("<h1>home page of project2<h1>")
+
+def html_demo1(request):
+    return render(request,"sample.html")
